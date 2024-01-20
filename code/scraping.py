572c9c7e97789b37ed17a78c0263fd8972bbeb83
Sérgio Filho <sergiovf_@hotmail.com>
2024-01-19 22:15:33 -0300

    fix(mongo path): fix mongo path in dictionary

--- code/scraping.py
+++ code/scraping.py
@@ -5,7 +5,7 @@ import xmltodict
 from pymongo import MongoClient
 
 path = {"root": Path(__file__).parent.parent.resolve()}
-path["files"] = path["root"] / "data"
+path["data"] = path["root"] / "data"
 credential = (path["data"] / "credential.txt").read_text().splitlines()
 mongo_uri = "mongodb+srv://{0}:{1}@{2}/{3}?retryWrites=true&w=majority".format(
     *credential
